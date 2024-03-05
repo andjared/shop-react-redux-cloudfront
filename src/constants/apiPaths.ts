1695f396f1ba36546f5f504c6169276af946140b
andjared <andja.velimirovic@gmail.com>
2024-03-05 13:36:33 +0100

    Link to import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-	product: 'https://e49oj0qmqc.execute-api.eu-west-1.amazonaws.com',
-	order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-	import: 'https://baqoiexga8.execute-api.eu-west-1.amazonaws.com/import',
-	bff: 'https://e49oj0qmqc.execute-api.eu-west-1.amazonaws.com',
-	cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  product: "https://e49oj0qmqc.execute-api.eu-west-1.amazonaws.com",
+  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://baqoiexga8.execute-api.eu-west-1.amazonaws.com/import",
+  bff: "https://e49oj0qmqc.execute-api.eu-west-1.amazonaws.com",
+  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
